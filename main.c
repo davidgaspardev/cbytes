6d495ee750d6c3a1be1e2d9501b0300358186175
David Gaspar <davidgaspar.dev@gmail.com>
2021-03-10 21:48:39 -0300

    Length function and comments added

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
+// Get length from a string (char array)
+unsigned int length(const char * string) {
+    unsigned int index = 0;
+
+    while (*(string + index) != '\0') ++index;
+    
+    return index;
+}
+
+// Entry point, It is the point at which execution 
+// of program is started
 int main() {
-    printf("Hello World!");
+
+    // Create a char array (pointer)
+    char * hello_world = "Hello World!";
+
+    printf("%s (length: %u)", hello_world, length(hello_world));
 
     return 1;
 }
