c670e2e73429b9392754644c9ecc2a793a4c562d
David Gaspar <davidgaspar.dev@gmail.com>
2021-10-28 16:41:24 -0300

    Indenting code (file: string.h)

--- src/string.h
+++ src/string.h
@@ -7,14 +7,14 @@
 
 #include <stdio.h>
 
-#define FALSE  0
-#define TRUE   1
+#define FALSE 0
+#define TRUE 1
 
-unsigned int length(const char * arg);
-unsigned int equal(const char * data_1, const char * data_2);
-void copy(const char * source, char * destination, size_t sizeof_destination);
-int subcopy(const char * source, int start_index, int end_index, char * destination,  size_t sizeof_destination);
-int subcopy_len(const char * src, unsigned int dest_length, char * dest, size_t sizeof_dest);
-int subcopy_index_len(const char * src, unsigned int start_index, unsigned int dest_length, char * dest, size_t sizeof_dest);
+unsigned int length(const char *arg);
+unsigned int equal(const char *data_1, const char *data_2);
+void copy(const char *source, char *destination, size_t sizeof_destination);
+int subcopy(const char *source, int start_index, int end_index, char *destination, size_t sizeof_destination);
+int subcopy_len(const char *src, unsigned int dest_length, char *dest, size_t sizeof_dest);
+int subcopy_index_len(const char *src, unsigned int start_index, unsigned int dest_length, char *dest, size_t sizeof_dest);
 
 #endif
