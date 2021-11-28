d7f0f13ec8772eab456801d50404b466598167c0
David Gaspar <davidgaspar.dev@gmail.com>
2021-11-28 15:45:28 -0300

    Test code added for copy function - close #6

--- tests/copy_test.c
+++ tests/copy_test.c
@@ -5,6 +5,20 @@
 void copy_test(const char * data) {
     log_title("TESTING COPY METHODS WITH ARGUMENT: %s", data);
 
+    int full_len = length(data);
+    char data_full_copy[full_len];
+
+    log_info("Copying the characters of the '%s' string to the data_copy variable", data);
+
+    if(copy(data, data_full_copy, sizeof(data_full_copy)) > -1) {
+        log_ok("data: %s", data);
+        log_ok("data_copy: " YEL "%s" RESET, data_full_copy);
+    } else {
+        log_err("something wrong with subcopy method");
+    }
+
+    log_title("TESTING COPY METHODS WITH ARGUMENT: %s", data);
+
     int len = length(data) / 2;
     char data_copy[len];
 
